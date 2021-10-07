f9f35d3a30ea3785d5d427f8ac442c19e4353b33
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-07 04:34:15 -0300

    Aula 3: Finalizada!

---
+++ loja-virtual-repository/src/br/com/alura/data/TesteInsercaoComParametro.java
@@ -0,0 +1,37 @@
+package br.com.alura.data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TesteInsercaoComParametro {
+
+	public static void main(String[] args) throws SQLException {
+		
+		String nome = "Monitor";
+		String descricao = "Monitor 4k";
+	
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.getConexao();
+		
+		PreparedStatement stm = connection.prepareStatement(
+				"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+				Statement.RETURN_GENERATED_KEYS);
+		
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		
+		stm.execute();
+		
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		while(rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("O id criado foi: " + id);
+		}
+	}
+	
+}
