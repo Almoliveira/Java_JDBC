2148f20967bf3c4275eda03b53e567c86b023d60
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-08 04:37:56 -0300

    Aula 5: Finalizada!

---
+++ loja-virtual-repository/src/br/com/alura/data/TestaPoolConexoes.java
@@ -0,0 +1,18 @@
+package br.com.alura.data;
+
+import java.sql.SQLException;
+
+public abstract class TestaPoolConexoes {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+
+		ConnectionFactory conexao = new ConnectionFactory();
+		
+		for (int i = 0 ; i < 20; i++) {
+			conexao.getConexao();
+			System.out.println("Conexão de numero:" +i);
+		}
+	}
+
+}
